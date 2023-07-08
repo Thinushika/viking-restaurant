5b4f0e8f51ab26685a67d13b58d6493f1eb5a0da
tj <thinupavani@gmail.com>
2023-07-08 18:22:02 +0530

    navbar mobile issue fix

--- src/components/nav_components/NavLayout.tsx
+++ src/components/nav_components/NavLayout.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ActiveNav from "./ActiveNav";
 import Link from "next/link";
 import Image from "next/image";
+import styles from '../../styles/Navbar.module.css'
+import SocialMedia from "./SocialMedia";
 
 interface NavLayoutProps {
   children?: React.ReactNode;
@@ -14,114 +16,113 @@ const NavLayout: React.FC<NavLayoutProps> = ({ children }) => {
     setShowMobileNav((current) => !current);
   };
 
+  const navLinks = [
+    {
+      id: 1,
+      title: 'Home',
+      link: '/'
+    },
+    {
+      id: 2,
+      title: 'Plan a Trip',
+      link: '/'
+    },
+    {
+      id: 3,
+      title: 'About',
+      link: '/'
+    },
+    {
+      id: 4,
+      title: 'Menu',
+      link: '/'
+    },
+    {
+      id: 5,
+      title: 'Booking',
+      link: '/'
+    },
+    {
+      id: 6,
+      title: 'Gallery',
+      link: '/'
+    },
+    {
+      id: 7,
+      title: 'Contact',
+      link: '/'
+    }
+  ]
+
   return (
     <>
-    <div className="p-0 m-0 position-relative over-hidd">
-      <nav className="navbar navbar-expand-xl main-padding padding-x-100px px-5 position-relative z999">
-        <div className="container-fluid px-0">
-          {/* side logo area */}
-          <div className="d-flex flex-row justify-content-center align-items-center">
-            <Link className="navbar-brand font-lg mb-0" href="#">
-              <Image
-                src={"/assests/images/icons/logo.png"}
-                alt=""
-                width={180}
-                height={100}
-              />
-            </Link>
-          </div>
-          {/* mobile toggle button */}
-          <button
-            className="navbar-toggler"
-            onClick={mobileNavbarHandler}
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="bi bi-list toggle-icon-styles"></span>
-          </button>
-          {showMoibleNav ? (
-            <>
-              <div className="d-flex flex-column mobile-nav-container text-center justify-content-start">
-                <div className="d-flex justify-content-end py-3 px-3">
-                  <i
-                    className="bi bi-x-lg close-icon-mobile-nav"
-                    onClick={mobileNavbarHandler}
-                  ></i>
-                </div>
-                <div className="d-flex flex-column text-center justify-content-center pt-5 mt-5">
-                  <Link
-                    className="nav-link px-3 py-2"
-                    aria-current="page"
-                    onClick={mobileNavbarHandler}
-                    href="/"
-                  >
-                    Home
-                  </Link>
-                  <Link
-                    className="nav-link px-3 py-2"
-                    onClick={mobileNavbarHandler}
-                    href="/projects"
-                  >
-                    Projects
-                  </Link>
-                  <Link
-                    className="nav-link px-3 py-2"
-                    onClick={mobileNavbarHandler}
-                    href="/services"
-                  >
-                    Services
-                  </Link>
-                  <Link
-                    className="nav-link px-3 py-2"
-                    onClick={mobileNavbarHandler}
-                    href="/market"
-                  >
-                    Market
-                  </Link>
-                  <Link
-                    className="nav-link px-3 py-2"
-                    onClick={mobileNavbarHandler}
-                    href="/about-us"
-                  >
-                    About Us
-                  </Link>
-                </div>
-              </div>
-              <ActiveNav />
-              {children}
-            </>
-          ) : null}
+      <div className={`p-0 m-0 px-lg-0 py-lg-3 position-relative ${styles.navbarDesign}`}>
+        <nav className="navbar navbar-expand-xl px-2 px-lg-5">
+          <div className="container-fluid flex-column flex-xl-row align-items-start align-items-xl-center px-0 px-lg-5">
 
-          {/* nav links area */}
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav ms-auto">
-              <Link className="nav-link px-3 py-0" aria-current="page" href="/">
-                Home
-              </Link>
-              <Link className="nav-link px-3 py-0" href="/projects">
-                Projects
-              </Link>
-              <Link className="nav-link px-3 py-0" href="/services">
-                Services
-              </Link>
-              <Link className="nav-link px-3 py-0" href="/market">
-                Market
-              </Link>
-              <Link className="nav-link px-3 py-0" href="/about-us">
-                About Us
-              </Link>
+            {/* mobile toggle button */}
+            <button
+              className="navbar-toggler"
+              onClick={mobileNavbarHandler}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="bi bi-list nav-toggle-icon"></span>
+            </button>
+            {showMoibleNav ? (
+              <>
+                <div className="d-flex flex-column text-center justify-content-start">
+                  {/* <div className="d-flex justify-content-end py-1 ">
+                    <i
+                      className="bi bi-x-lg close-icon-mobile-nav"
+                      onClick={mobileNavbarHandler}
+                    ></i>
+                  </div> */}
+                  <div className="d-flex flex-column text-start justify-content-center pt-0 mb-5">
+                    {
+                      navLinks.map((item) => (
+                        <Link
+                          key={item.id}
+                          className={`nav-link ${styles.navLink} px-3 py-2`}
+                          aria-current="page"
+                          onClick={mobileNavbarHandler}
+                          href={item.link}
+                        >
+                          {item.title}
+                        </Link>
+                      ))
+                    }
+                    <SocialMedia />
+                  </div>
+                </div>
+                <ActiveNav />
+                {children}
+              </>
+            ) : null}
+
+            {/* nav links area */}
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav d-flex justify-content-between w-100">
+                {
+                  navLinks.map((item) => (
+                    <Link key={item.id} className={`nav-link ${styles.navLink} px-3 ps-lg-0 pe-lg-5 py-0`} href={item.link}>
+                      {item.title}
+                    </Link>
+                  ))
+                }
+                <SocialMedia />
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-      <ActiveNav />
-      
-      {children}
-    </div>
+        </nav>
+        <ActiveNav />
+
+        {children}
+      </div>
     </>
   );
 };
