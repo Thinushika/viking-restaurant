ed1a9448cd9a2cfc8fd5ff85d2311de35ad7c5a9
tj <thinupavani@gmail.com>
2023-09-08 08:07:37 +0530

    update

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -137,8 +137,8 @@ const AboutUs = () => {
           <div className="d-flex flex-column justify-content-center align-items-center p-0 p-lg-5 m-0 w-100">
             <p className={`${styles.section_subtitle}`}>Galary</p>
             <h3 className={`${styles.section_title} mb-4`}>What We Make</h3>
-            <div className={`d-flex flex-column flex-lg-row p-0 m-0 px-lg-5 w-100 text-center`}>
-              <div className={`col-12 col-md-6 col-lg-3 p-1 ${styles.gallery_sec_height} ${styles.column_small}`}>
+            <div className={`d-flex flex-column flex-lg-row p-0 m-0 px-lg-2 w-100 text-center`}>
+              <div className={`col-12 col-md-6 col-lg-3 pe-1 ${styles.gallery_sec_height} ${styles.column_small}`}>
               <Image
                   src={"/assests/images/gallery/1.jpg"}
                   alt=""
@@ -154,16 +154,16 @@ const AboutUs = () => {
                   className={`img-fluid ${styles.team_image}`}
                 />
               </div>
-              <div className={`col-12 col-md-6 col-lg-3 p-1 ${styles.gallery_sec_height} ${styles.column_lg}`}>
+              <div className={`col-12 col-md-6 col-lg-6 p-1 ${styles.gallery_sec_height} ${styles.column_lg}`}>
               <Image
-                  src={"/assests/images/gallery/6.png"}
+                  src={"/assests/images/gallery/cropped.png"}
                   alt=""
                   width={350}
                   height={300}
                   className={`img-fluid ${styles.team_image} mb-2`}
                 />
               </div>
-              <div className={`col-12 col-md-6 col-lg-3 p-1 ${styles.gallery_sec_height} ${styles.column_small}`}>
+              <div className={`col-12 col-md-6 col-lg-3 ps-1 ${styles.gallery_sec_height} ${styles.column_small}`}>
               <Image
                   src={"/assests/images/gallery/4.jpg"}
                   alt=""
